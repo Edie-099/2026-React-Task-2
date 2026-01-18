35645dfb58e19e86b963c8f97c05f05489833c13
toku <edie992100@gmail.com>
2026-01-18 19:47:18 +0800

    修正路徑

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/2026-React-Task-2/" : "/",
   plugins: [react()],
-  base: "/2026-React-Task-2/",
-});
+})
